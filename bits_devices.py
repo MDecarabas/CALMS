a6a015e0684e21574193668599dfe93e355aaa0e
Eric Codrea <codrea.eric@gmail.com>
2025-01-29 10:34:38 -0600

    added bits sim

--- bits_devices.py
+++ bits_devices.py
@@ -9,3 +9,6 @@ class MyEpicsMotor(EpicsMotor):
 
 m1 = MyEpicsMotor("eac99:m1", name="m1", labels=("motor",))
 m2 = MyEpicsMotor("eac99:m2", name="m2", labels=("motor",))
+
+
+from ophyd.sim import noisy_det as sim_det # This is the detector you should use
